27208640d0e2ea9eb0159aa186911214cb2b7313
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-02-01 22:27:22 +0100

    move from xml-builder to xml-builder-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,30 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import { AppRoutes, routes } from "./Routes";
-import BasicLayout from "./Layout/BasicLayout";
+import { HashRouter } from "react-router-dom";
+import { AppRoutes } from "./Routes";
 
 import "./App.css";
 import "./App.scss";
+import BasicLayout from "./Pages/Layout/BasicLayout";
+import OrganizationContextLoader from "./SmartComponents/OrganizationContextLoader";
+import DeleteMessageDialog from "./SmartComponents/DeleteDialog";
+
+import '@redhat-cloud-services/frontend-components-notifications/index.css';
+const frontendComponentsNotifications = require("@redhat-cloud-services/frontend-components-notifications");
 
 const App: React.FC = () => {
+  const NotificationsPortal = frontendComponentsNotifications.NotificationsPortal;
+
   return (
     <React.Fragment>
-      <BrowserRouter>
-        <BasicLayout sidebar={routes}>
-          <AppRoutes />
-        </BasicLayout>
-      </BrowserRouter>
+      <HashRouter>
+        <OrganizationContextLoader>
+          <BasicLayout>
+            <AppRoutes />
+            <DeleteMessageDialog />
+            <NotificationsPortal />
+          </BasicLayout>
+        </OrganizationContextLoader>
+      </HashRouter>
     </React.Fragment>
   );
 };

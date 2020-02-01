7c0b4c47dddcc1c587f039b1af1331e07c926b17
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-02-01 22:24:29 +0100

    last original commit

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, RouteComponentProps, Switch } from "react-router-dom";
 import WelcomePage from "./Pages/WelcomePage";
 import NotFoundPage from "./Pages/NotFoundPage";
+import StandardDocumentPage from "./Pages/StandardDocumentPage";
 
 export interface IAppRoute {
   path: string;
@@ -9,21 +10,29 @@ export interface IAppRoute {
     | React.ComponentType<RouteComponentProps<any>>
     | React.ComponentType<any>;
   sidebarLabel?: string;
+  exact: boolean;
 }
 
 export const routes: IAppRoute[] = [
   {
     path: "/",
     component: WelcomePage,
-    sidebarLabel: "Bienvenido"
+    sidebarLabel: "Bienvenido",
+    exact: true
+  },
+  {
+    path: "/documents/standard",
+    component: StandardDocumentPage,
+    sidebarLabel: "Documentos estandar",
+    exact: true
   }
 ];
 
 export const AppRoutes = () => {
   return (
     <Switch>
-      {routes.map(({ path, component }, idx) => (
-        <Route path={path} component={component} key={idx} />
+      {routes.map(({ path, component, exact }, idx) => (
+        <Route path={path} component={component} exact={exact} key={idx} />
       ))}
       <Route component={NotFoundPage} />
     </Switch>

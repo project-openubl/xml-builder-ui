42b885ccf76e463502f2dcf5039ab947a3f242f5
Carlos Esteban Feria Vila <2582866+carlosthe19916@users.noreply.github.com>
2020-02-14 18:13:38 +0100

    remove warning

--- src/PresentationalComponents/PageOrganizationDocuments/PageOrganizationDocuments.tsx
+++ src/PresentationalComponents/PageOrganizationDocuments/PageOrganizationDocuments.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import { XmlBuilderRouterProps } from "../../models/routerProps";
 import { PageCreateStandardDocument } from "./PageCreateStandardDocument";
 import { BajaDocumentPage } from "./PageCreateVoidedDocument";
